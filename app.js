3921c97d8d564a4783479e28a1b45414dc2409c1
juui <89135410+Jjuuuuu@users.noreply.github.com>
2022-03-19 02:49:45 +0900

    ~2.16 recap
    learn how to write javascript at beginner's level

--- app.js
+++ app.js
@@ -16,36 +16,67 @@ plus(); // 4
 
 function plus(a, b) {
     console.log(a + b);
-};
+}; // NaN
 plus(1, 2); // 3
 
 // calculator
 const calculator = {
     add: function(a, b){
-        console.log(a + b);
+        return a + b;
     },
     subtract: function(a, b) {
-        console.log(a - b);
+        return a - b;
     },
     multiply: function(a, b) {
-        console.log(a * b);
+        return a * b;
     },
     divide: function(a, b) {
-        console.log(a / b);
+        return a / b;
     },
     powerOf: function(a, b) {
-        console.log( a ** b);
+        return a ** b;
     },
+    
 };
 
-let a = 4
-let b = 8
 
-calculator.add(a, b); // 12
-calculator.subtract(a, b); // -4
-calculator.multiply(a, b); // 32
-calculator.divide(a, b); // 0.5
-calculator.powerOf(a, b); // 65536
+const plusResult = calculator.add(1 + 2) // 3
 
 
 
+// conditionals
+const age = prompt("How old are you?"); // <-- Not using anymore because it pauses javascript execution
+
+console.log(typeof "15", parseInt(15)); // string number
+console.log(typeof age, parseInt(age)); // string NaN(Not a Number)
+// const age = parseInt(prompt("How old are you?")); 
+console.log(isNaN(age));
+
+if(isNaN(age) || age < 0){
+    /* condition === true
+     * condition === boolean
+     * isNaN()
+     */
+    console.log("Please write a real positive number")
+ } else if (age < 18) {
+    // condition === true
+    console.log("you are too young");
+ } else if (age >= 18 && age <= 80) {
+    /* && and
+     * true && true === true
+     * false && true === false
+     * || or
+     * true || true === true
+     * true || false === true
+     * false || true === true
+     * false || false === false
+     */
+    console.log("you are good to drink")
+} else if (age === 100) {
+    console.log("Wow You are wise")
+} else if (age > 80) {
+    // condition === false
+    console.log("You can't drink")
+    }
+
+ 
