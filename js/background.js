19f50dfa32d66aee021b9cb4638a182cd001001a
Jahye Hwang <meltinp@gmail.com>
2024-09-26 14:09:47 +0900

    240926 메뉴 입력 가능하게 수정

--- js/background.js
+++ js/background.js
@@ -1,13 +1,25 @@
-const images = ["bg1.jpg", "bg2.jpg", "bg3.jpg"];
+const colors = [
+  "#f0f8ff", // Alice Blue
+  "#fafad2", // Light Goldenrod Yellow
+  "#ffe4e1", // Misty Rose
+  "#e6e6fa", // Lavender
+  "#ffefd5", // Papaya Whip
+  "#f5f5dc", // Beige
+  "#ffe4b5", // Moccasin
+  "#d8bfd8", // Thistle
+  "#add8e6", // Light Blue
+  "#b0e0e6", // Powder Blue
+  "#ffdab9", // Peach Puff
+  "#e0ffff", // Light Cyan
+  "#f5fffa", // Mint Cream
+  "#f0e68c", // Khaki
+  "#faf0e6", // Linen
+];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+// 두 가지 색상을 랜덤으로 선택
+const color1 = colors[Math.floor(Math.random() * colors.length)];
+const color2 = colors[Math.floor(Math.random() * colors.length)];
 
-// const bgImage = document.createElement("img");
-
-// bgImage.src = `img/${chosenImage}`;
-
-// document.body.appendChild(bgImage);
-
-bgImageSrc = `img/${chosenImage}`;
-
-document.body.style.backgroundImage = `url(${bgImageSrc})`;
+// linear-gradient로 두 색상을 배경에 적용
+document.body.style.backgroundImage = `linear-gradient(to right, ${color1}, ${color2})`;
+document.body.style.color = "black"; // 텍스트 색상을 블랙으로 설정
